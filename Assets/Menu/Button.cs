22aad580e77bfec40c1966141ac50573d9e1cca5
panthevm <panthevm@yandex.ru>
2020-04-19 13:39:14 +0300

    Init

---
+++ Assets/Menu/Button.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class Event : MonoBehaviour {
+    public void OnPress(Hand hand) {
+        Debug.Log("SteamVR Button pressed!");
+    }
+    public void OnCustomButtonPress() {
+        Debug.Log("We pushed our custom button!");
+    }
+}
