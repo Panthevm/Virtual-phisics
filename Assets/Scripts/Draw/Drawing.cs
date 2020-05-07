34e3b03e4d7fe1f339ec9df19f0eb175592eb67b
panthevm <panthevm@yandex.ru>
2020-05-07 09:38:35 +0300

    Draw script WIP

---
+++ Assets/Scripts/Draw/Drawing.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using Valve.VR;
+
+public class Drawing : BaseInputModule
+{
+    public SteamVR_Input_Sources m_TargetSource;
+    public SteamVR_Action_Boolean m_ClickAction;
+
+    public override void Process()
+    {
+
+        if (m_ClickAction.GetStateDown(m_TargetSource))
+            ProccessPress(1);
+    }
+
+    private void ProccessPress(int data) {
+        Debug.Log("Press");
+    }
+}
